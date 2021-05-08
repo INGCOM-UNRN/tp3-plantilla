a90e0209f3ca3087b0f4fa5e0c0d5675ff859001
GastonPrat <83458328+GastonPrat@users.noreply.github.com>
2021-05-08 19:56:19 -0300

    Update prat_gaston_grep1.sh

--- prat_gaston_grep1.sh
+++ prat_gaston_grep1.sh
@@ -9,3 +9,16 @@ grep -E [[:digit:]]{3}[[:space:]] grepdata.txt
 
 echo "(3) Imprime todas las líneas que contienen una fecha."
 grep -E [[:space:]]2[[:digit:]]{3} grepdata.txt
+
+echo "(4) Imprime todas las líneas que contienen una vocal (a, e, i, o o u) seguidas de un solo carácter seguido de la misma vocal nuevamente."
+grep -iE [a][[:alpha:]][a] grepdata.txt
+grep -iE [e][[:alpha:]][e] grepdata.txt
+grep -iE [i][[:alpha:]][i] grepdata.txt
+grep -iE [o][[:alpha:]][o] grepdata.txt
+grep -iE [u][[:alpha:]][u] grepdata.txt
+
+echo "(5) Imprima todas las líneas que no comiencen con una S mayúscula."
+grep -v ^S grepdata.txt
+
+echo"(6) Imprima todas las lineas que contengan una dirección de correo electrónica."
+grep "@" grepdata.txt 
