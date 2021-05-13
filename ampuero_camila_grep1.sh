7b6f8f5a2c264f73dcde6970008e6d730312cb88
Camila Sol Ampuero <83739156+CamilaSol@users.noreply.github.com>
2021-05-12 23:12:10 -0300

    Update and rename README.md to ampuero_camila_grep1.sh

---
+++ ampuero_camila_grep1.sh
@@ -0,0 +1,33 @@
+echo "Trabajo practico número 3" Camila Ampuero
+
+Consigna 1
+
+grep 'x[[:digit:]][[:digit:]][[:digit:]][[:digit:]]' grepdata.txt
+
+consigna 2
+
+grep -E "^[[:digit:]][[:digit:]][[:digit:]][[:space:]]{1}" grepdata.txt
+
+consigna 3
+
+grep -E '[[:space:]]2[[:digit:]]{3}' grepdata.txt
+
+consigna 4
+
+grep -iE '[a][[:alpha:]][a]' grepdata.txt
+
+grep -iE '[e][[:alpha:]][e]' grepdata.txt
+
+grep -iE '[i][[:alpha:]][i]' grepdata.txt
+
+grep -iE '[o][[:alpha:]][o]' grepdata.txt
+
+grep -iE '[u][[:alpha:]][u]' grepdata.txt
+
+consigna 5
+
+grep -v '^S' grepdata.txt
+
+consigna 6
+
+grep "@" grepdata.txt
